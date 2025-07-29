bbd9a6e23ab7626926c939d7d73c042fae29272b
Nossi <121175399+N0ssi@users.noreply.github.com>
2025-07-29 14:44:32 +0200

    javascript test

---
+++ public/template.js
@@ -0,0 +1,19 @@
+const template = document.createElement('titolo');
+
+template.innerHTML = `
+        <img src="nossiland.png" href="index.html">
+            <p class="title">Nostalgic <b>bastard</b> :)</p>
+
+      <nav>
+    <ul>
+      <li><a href="update.html">Update</a></li>
+      <li><a href="contact.asp">Fun and ames</a></li>
+      <li><a href="about.asp">Muzack</a></li>
+    </ul> 
+    </nav>
+
+
+
+`;
+
+document.body.appendChild(titolo.content);
