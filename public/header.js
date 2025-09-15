0d3ba5c93130830f0f528106b5f4c2a4133fca25
Nossi <121175399+N0ssi@users.noreply.github.com>
2025-09-15 14:49:54 +0200

    owo

--- public/header.js
+++ public/header.js
@@ -7,9 +7,10 @@ header.innerHTML = `
   <p class="title">Nostalgic <b>bastard</b> :)</p>
   <nav>
     <ul>
-      <li><a href="update.html">Update</a></li>
-      <li><a href="contact.asp">Fun and ames</a></li>
+      <li><a href="contact.asp">Stealthtank</a></li>
       <li><a href="about.asp">Muzack</a></li>
+        <li><a href="update.html">Chronology :)</a></li>
+
     </ul>
   </nav>
 `;
